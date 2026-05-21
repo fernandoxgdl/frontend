d8e0efe0de270579fb53e99b7c2e3ce811e82369
armando <fernandoxgdl@gmail.com>
2026-05-21 02:23:38 -0600

    correciones a CSP, eliminando tags duplicadas

--- src/pages/UbicacionG.js
+++ src/pages/UbicacionG.js
@@ -12,9 +12,9 @@ function Vision() {
           loading="lazy"
           allowFullScreen
           referrerPolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4070.0036527424195!2d-103.37902844497623!3d20.701703263680272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae36cd492c0d%3A0x32f3c48a45392ef8!2sPunto%20Sao%20Paulo!5e0!3m2!1ses-419!2smx!4v1779344296519!5m2!1ses-419!2smx
-"
-        ></iframe>
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCq_gUE1Ln1pA8Tl3e2fETMxDqR_zTHr4c&q=Punto+Sao+Paulo+Zapopan&zoom=15">
+
+          </iframe>
       </div>
 
       <div className="info-ubicacion">
