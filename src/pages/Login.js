7227f7969dc5e32354e5656adcc2615cde8ca7b3
armando <fernandoxgdl@gmail.com>
2026-05-24 13:59:51 -0600

    fix type error

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,13 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-function Login({setEsAdmin}) {
-  
+function Login({ setEsAdmin }) {
   const [password, setPassword] = useState("");
   const [mensaje, setMensaje] = useState("");
   const [email, setEmail] = useState("");
   const [cargando, setCargando] = useState(false);
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -17,52 +15,57 @@ function Login({setEsAdmin}) {
     setMensaje("");
 
     try {
-      const respuesta = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify({ email, password }),
-      });
-    
-      const data = await respuesta.json();
-      
-      if (respuesta.ok) {
+      const respuesta = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
         setEsAdmin(true);
         navigate("/admin");
       } else {
-        setMensaje(data.error || "credenciale incorrectas");
+        setMensaje(data.err || "credenciale incorrectas");
       }
-    } catch (error) {
+    } catch (err) {
       setMensaje("Error de conexión");
     } finally {
       setCargando(false);
     }
   };
 
-
   return (
-    <div style={{ padding:"20px", 
-     textAlign:'center',
-     minHeight: '70vh',
-     width:'60vw',
-     position: 'relative',
-     marginRight:'auto',
-     marginLeft:'auto',
-     marginTop:'20px',
-     display:'flex',
-     flexDirection:'column',
-     rowGap:'20px'}}>
-      
+    <div
+      style={{
+        padding: "20px",
+        textAlign: "center",
+        minHeight: "70vh",
+        width: "60vw",
+        position: "relative",
+        marginRight: "auto",
+        marginLeft: "auto",
+        marginTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "20px",
+      }}
+    >
       <h2>Login del Sistema</h2>
 
       <form onSubmit={handleLogin}>
         <div>
           <input
             type="text"
-            placeholder="Usuario"
-            value={usuario}
-            onChange={(e) => setUsuario(e.target.value)}
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
